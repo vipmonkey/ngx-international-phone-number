b57352905d048dd94f82bf77a5798baf4b10796c
Dennis <dennis@vipmonkey.se>
2019-03-25 10:12:03 +0100

    changed to angular material

--- dist/country.pipe.d.ts
+++
@@ -1,5 +0,0 @@
-import { PipeTransform } from '@angular/core';
-import { Country } from './country.model';
-export declare class CountryPipe implements PipeTransform {
-    transform(value: Country[], args?: string): any;
-}
--- dist/only-number.directive.d.ts
+++
@@ -1,8 +0,0 @@
-import { ElementRef } from '@angular/core';
-export declare class OnlyNumberDirective {
-    private el;
-    onlyNumber: boolean;
-    regexStr: string;
-    constructor(el: ElementRef);
-    onKeyDown(event: KeyboardEvent): void;
-}
